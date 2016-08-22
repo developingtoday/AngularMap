20a788d38ce08a8e598eafbff43298e38964a7cd
developingtoday <developingtoday@gmail.com>
2016-08-22 21:54:50 +0300

    update of the bootsrtap.

--- app/main.js
+++ app/main.js
@@ -1,13 +1,6 @@
 "use strict";
-var core_1 = require('@angular/core');
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_component_1 = require('./app.component');
-var core_2 = require('angular2-google-maps/core');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [core_2.GOOGLE_MAPS_PROVIDERS], core_1.provide(core_2.LazyMapsAPILoaderConfig, {
-    useFactory: function () {
-        var config = new core_2.LazyMapsAPILoaderConfig();
-        config.apiKey = "AIzaSyCN8k9JNzct9wajoOoplow8Rpu9TyLp7Uc";
-        return config;
-    }
-}));
+var core_1 = require('angular2-google-maps/core');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [core_1.GOOGLE_MAPS_PROVIDERS]);
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -3,10 +3,4 @@ import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 import {GOOGLE_MAPS_PROVIDERS,LazyMapsAPILoaderConfig} from 'angular2-google-maps/core'; 
 
-bootstrap(AppComponent,[GOOGLE_MAPS_PROVIDERS],provide(LazyMapsAPILoaderConfig,{
-useFactory:()=>{
-    let config=new LazyMapsAPILoaderConfig();
-    config.apiKey="AIzaSyCN8k9JNzct9wajoOoplow8Rpu9TyLp7Uc";
-    return config;
-}
-}));
+bootstrap(AppComponent,[GOOGLE_MAPS_PROVIDERS]);
